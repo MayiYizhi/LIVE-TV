8bf186d47d53a8b9b92f023a211e28e587054e7f
gohkh0825 <kahhanu0857@gmail.com>
2022-05-09 09:07:53 +0800

    自动更新

--- 01.sh
+++ 01.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 cd /home/iptv
+git init
+git add README.md
 git add . 
 git commit -m '自动更新' 
-git push origin master
+#git fetch origin master
+#git merge origin master
+#git fetch origin master:tmp
+#git rebase tmp
+#git push origin HEAD:master
+#git branch -D tmp
+git remote add origin git@github.com:gohkh0825/LIVE-TV.git
+git push --force origin master
